7f9913b861980607d7a3d85fe10775f0a46bc6a9
Melisa Salazar <67084450+melisaailensalazar@users.noreply.github.com>
2024-05-18 00:19:06 -0300

    Manzanas

--- src/app/apple/components/buttonNote.tsx
+++ src/app/apple/components/buttonNote.tsx
@@ -2,31 +2,30 @@
 
 import React from 'react';
 import Image from 'next/image';
-import appleImage from "@/../public/img/apples/apple.png";
+import noteImage from "@/../public/img/apples/nota.png";
 
-interface AppleProps {
-    letter: string;
+interface NoteProps {
     onClick: () => void;
+    className?: string;
 }
 
-const ButtonApple: React.FC<AppleProps> = ({ letter, onClick }) => {
+const ButtonNote: React.FC<NoteProps> = ({ onClick }) => {
     return (
         <div
             className="relative w-12 h-12 m-1 cursor-pointer"
             onClick={onClick}
         >
             <Image
-                src={appleImage}
+                src={noteImage}
                 alt="Apple"
                 layout="fill"
                 objectFit="cover"
                 className="w-full h-full"
             />
             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-lg z-10">
-                {letter}
             </div>
         </div>
     );
 };
 
-export default ButtonApple;
+export default ButtonNote;
--- src/app/apple/page.tsx
+++ src/app/apple/page.tsx
@@ -1,25 +1,47 @@
-'use client'; 
-
+"use client"
+// ApplePath.tsx
 import React from 'react';
 import ButtonApple from './components/buttonApple';
+import NoteImage from './components/buttonNote';
 
 const ApplePath: React.FC = () => {
-  const handleClick = (letter: string) => {
-    console.log(`Clicked on apple with letter ${letter}`);
-    // Aquí puedes manejar la lógica de lo que sucede cuando se hace clic en una manzana
-  };
+    const handleClick = (letter: string) => {
+        console.log(`Clicked on apple with letter ${letter}`);
+    };
 
+    const sequence: (string | null)[][] = [
+        ["A", null, null],
+        [null, "N", null],
+        [null, null, "E"],
+        [null, "N", null],
+        ["I", null, null],
+        [null, "N", null],
+        [null, null, "O"],
+        [null, "N", null],
+        ["U", null, null],
+        [null, "N", null]
+    ];
 
-  const alphabet = 'AEIOUMPBVSLNTRYFKDHJVZCGWX';
-                   
-  return (
-    
-    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-      {alphabet.split('').map((letter, index) => (
-        <ButtonApple key={index} letter={letter} onClick={() => handleClick(letter)} />
-      ))}
-    </div>
-   
-  );
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <div style={{ width: '200px' }}>
+                {sequence.map((row, rowIndex) => (
+                    <div key={rowIndex} style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        {row.map((item, colIndex) => (
+                            <div key={colIndex}>
+                                {item === "A" && <ButtonApple letter={item} onClick={() => handleClick(item)} />}
+                                {item === "N" && <NoteImage onClick={() => {}} />}
+                                {item === "E" && <ButtonApple letter={item} onClick={() => handleClick(item)} />}
+                                {item === "I" && <ButtonApple letter={item} onClick={() => handleClick(item)} />}
+                                {item === "O" && <ButtonApple letter={item} onClick={() => handleClick(item)} />}
+                                {item === "U" && <ButtonApple letter={item} onClick={() => handleClick(item)} />}
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
+
 export default ApplePath;
