49547d6ddba91dbc72235fedc582bc75ad23c6c6
LJVazquez <ljvazquez00@gmail.com>
2024-05-18 13:48:56 -0300

    test: adds services tests

---
+++ src/app/(root)/[player]/modules/__test__/modules.service.test.jsx
@@ -0,0 +1,32 @@
+import { getModules } from '../services/modules.service';
+
+beforeAll(() => {
+	global.fetch = jest.fn();
+});
+
+describe('Modules service', () => {
+	it('makes a request to modules internal api with correct params', () => {
+		const mockResponse = {
+			data: { modules: [{ id: 1, description: 'module' }] },
+		};
+		const mockHeaders = {
+			'Content-Type': 'application/json',
+			Authorization: 'Bearer token',
+		};
+
+		fetch.mockResolvedValueOnce({
+			json: async () => mockResponse,
+		});
+
+		const response = getModules(1, 'token');
+
+		expect(fetch).toHaveBeenCalledWith(
+			expect.stringContaining(`/modules?playerId=1`),
+			{
+				headers: mockHeaders,
+				method: 'GET',
+			}
+		);
+		expect(response).resolves.toEqual(mockResponse.data.modules);
+	});
+});
