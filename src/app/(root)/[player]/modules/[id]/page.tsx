7617275000dbb3ea583a1c02fac8c9681a861558
LJVazquez <ljvazquez00@gmail.com>
2024-05-19 15:26:28 -0300

    chore: adds mock data to modules

--- src/app/(root)/[player]/modules/[id]/page.tsx
+++ src/app/(root)/[player]/modules/[id]/page.tsx
@@ -4,15 +4,51 @@ import { getApples } from './services/apples.service';
 
 export default async function ApplePath({ params }: any) {
 	const session = await getServerSession(options);
-	const player = session?.user.players.find(
-		(player) => player.alias === params.player
-	)!;
+	// const player = session?.user.players.find(
+	// 	(player) => player.alias === params.player
+	// )!;
 
-	const apples = await getApples(
-		player.id,
-		params.id,
-		session?.user?.accessToken!
+	// const apples = await getApples(
+	// 	player.id,
+	// 	params.id,
+	// 	session?.user?.accessToken!
+	// );
+
+	const mockApples = [
+		[
+			{ id: 7, name: 'BL', stars: 0 },
+			{ id: 8, name: 'BR', stars: 0 },
+			{ id: 9, name: 'CL', stars: 0 },
+			{ id: 10, name: 'CR', stars: 0 },
+			{ id: 11, name: 'CH', stars: 0 },
+		],
+		[
+			{ id: 7, name: 'BL', stars: 3 },
+			{ id: 8, name: 'BR', stars: 0 },
+			{ id: 9, name: 'CL', stars: 0 },
+			{ id: 10, name: 'CR', stars: 0 },
+			{ id: 11, name: 'CH', stars: 0 },
+		],
+		[
+			{ id: 7, name: 'BL', stars: 3 },
+			{ id: 8, name: 'BR', stars: 1 },
+			{ id: 9, name: 'CL', stars: 0 },
+			{ id: 10, name: 'CR', stars: 0 },
+			{ id: 11, name: 'CH', stars: 0 },
+		],
+	];
+
+	return (
+		<main>
+			{mockApples[Number(params.id) - 1].map((apple: any) => (
+				<button
+					className="bg-primary p-2 m-2 flex flex-col items-center"
+					key={apple.id}
+				>
+					<span>Manzana de ejemplo {apple.name}</span>
+					<span>{'⭐'.repeat(apple.stars)}</span>
+				</button>
+			))}
+		</main>
 	);
-
-	return <div>{JSON.stringify(apples)}</div>;
 }
