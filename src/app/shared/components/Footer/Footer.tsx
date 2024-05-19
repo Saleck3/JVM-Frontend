50af9f825219b61bb4b3acd238cf2a8f5dca4fd2
Saleck3 <aledagonale@gmail.com>
2024-05-19 16:46:05 -0300

    Vuelta a footer original

--- src/app/shared/components/Footer/Footer.tsx
+++ src/app/shared/components/Footer/Footer.tsx
@@ -5,29 +5,27 @@ import Debug from '../Debug/Debug'
 
 export default function Footer() {
 	return (
-		<div>
-			<footer className="sticky top-[100vh]">
-				<div className="bg-primary text-white py-12 pt-20 px-4 md:px-6">
-					<div className="container mx-auto flex flex-col md:flex-row items-center justify-between gap-8">
-						<div className="flex items-center gap-2">
-							<span className="text-lg font-medium">Lecti</span>
-						</div>
-						<nav className="flex flex-wrap items-center gap-4 md:gap-6">
-						</nav>
-						<div className="text-sm">
-							© 2024 Lecti. Todos los derechos reservados.
-						</div>
-					</div>
-				</div>
-				<Debug />
-			</footer>
+		<footer className="sticky top-[100vh]">
 			<Image
 				src={`${process.env.NEXT_PUBLIC_BASE_PATH}/img/footer-img.svg`}
 				alt="lecti"
 				width={400}
 				height={400}
-				className="mx-auto sticky top-[100vh]"
+				className="transform translate-y-1/4 mx-auto"
 			/>
-		</div>
+			<div className="bg-primary text-white py-12 pt-20 px-4 md:px-6">
+				<div className="container mx-auto flex flex-col md:flex-row items-center justify-between gap-8">
+					<div className="flex items-center gap-2">
+						<span className="text-lg font-medium">Lecti</span>
+					</div>
+					<nav className="flex flex-wrap items-center gap-4 md:gap-6">
+					</nav>
+					<div className="text-sm">
+						© 2024 Lecti. Todos los derechos reservados.
+					</div>
+				</div>
+			</div>
+			<Debug />
+		</footer>
 	);
 }
