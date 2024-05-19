c9d5d77274f51952bdf15b2777fde0c56db78ecd
LJVazquez <ljvazquez00@gmail.com>
2024-05-19 16:24:12 -0300

    hotfix: missing login link

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -26,13 +26,15 @@ export default function Login() {
 	return (
 		<main className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
 			<div className="sm:mx-auto sm:w-full sm:max-w-sm">
-				<Image
-					src="/img/hero_img.png"
-					alt="logo"
-					width={100}
-					height={100}
-					className="mx-auto"
-				/>
+				<Link href="/">
+					<Image
+						src="/img/hero_img.png"
+						alt="logo"
+						width={100}
+						height={100}
+						className="mx-auto"
+					/>
+				</Link>
 
 				<h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
 					Ingresá a tu cuenta
