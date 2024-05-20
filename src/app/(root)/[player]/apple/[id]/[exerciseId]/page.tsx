addd079239fccfe977045e78e0eb049745d38dd4
Saleck3 <aledagonale@gmail.com>
2024-05-19 21:02:37 -0300

    switch for harcode

--- src/app/(root)/[player]/apple/[id]/[exerciseId]/page.tsx
+++ src/app/(root)/[player]/apple/[id]/[exerciseId]/page.tsx
@@ -1,21 +1,53 @@
+import ChooseWord from "./games/chooseword"
+import WriteWord from "./games/writeword"
+import DropSyllable from "./games/dropSyllable"
 
-export default async function Apple({ params }: any) {
+export default function Apple({ params }: any) {
     switch (params.id) {
         case "1":
-            switch (params.id) {
+            switch (params.exerciseId) {
                 case "1":
                     return (
                         <div>
-                            <h1>Manzana {params.id}</h1>
-                            <h1>Ejercicio {params.exerciseId}</h1>
+                            <ChooseWord
+                                words={['ARGENTINA', 'AJO', 'ABRAZO', 'ALMA']}
+                                correctWord={"AJO"}
+                                image={'/img/games/ajo.jpg'}
+                                params={params}
+                            />
+
                         </div>
                     )
+                case "2":
+                    return (
+                        <div>
+                            <WriteWord
+                                correctWord={"leon"}
+                                params={params}
+                                image={'/img/games/lion.svg'}
+                            />
+                        </div>
+                    )
+                case "3":
+                    return (
+                        <DropSyllable
+                            syllables={['le', 'ón']}
+                            correctWord={"león"}
+                            params={params}
+                            image={'/img/games/lion.svg'}
+                        />
+                    )
+                default:
+
             }
         case "2":
             return (
                 <div>
-                    <h1>Manzana {params.id}</h1>
-                    <h1>Ejercicio {params.exerciseId}</h1>
+                    <WriteWord
+                        correctWord={"leonidas"}
+                        params={params}
+                        image={'/img/games/lion.svg'}
+                    />
                 </div>
             )
         case "3":
