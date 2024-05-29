2eee6847abde64eb6b7c37ff9f5208f3d5fb7357
Saleck3 <aledagonale@gmail.com>
2024-05-28 22:23:05 -0300

    Intento de camino sin grilla

--- src/app/(root)/[player]/modules/[id]/components/ButtonApple.tsx
+++ src/app/(root)/[player]/modules/[id]/components/ButtonApple.tsx
@@ -8,21 +8,27 @@ type Props = {
     playerAlias: string;
     appleId: number;
     stars: number;
+    index: number;
 }
 
-export default function ButtonApple({ name, playerAlias, appleId, stars }: Props): JSX.Element {
+export default function ButtonApple({ name, playerAlias, appleId, stars, index }: Props): JSX.Element {
 
+    const horizontalPosition = index % 2 === 0 ? 25 : 55; 
+    const verticalPosition = index * 200; // Each apple is 200px apart vertically
     return (
 
-        <Link className="relative w-full pt-[100%] overflow-hidden" href={`/${playerAlias}/apple/${appleId}/1`} >
-            <li className="absolute top-0 h-full m-[5px] cursor-pointer">
-                <Image
+        <Link className="absolute"
+            href={`/${playerAlias}/apple/${appleId}/1`}
+            style={{ top: `${verticalPosition}px`, left: `${horizontalPosition}%` }}
+            >
+            <li className="relative w-40 sm:w-60 md:w-80 overflow-hidden">
+                                <Image
                     className="max-w-full max-h-full object-cover rounded-lg"
                     src={'/img/modules/apple.svg'}
                     alt={"Apple_" + name}
                     height={400}
                     width={400} />
-                <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-[calc(4vw+4vh+2vmin)] font-bold '>
+                <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-[calc(3vw+3vh+2vmin)] font-bold '>
                     {name}
                 </div>
             </li>
