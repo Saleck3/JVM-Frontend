07896916f6ddd871a34c9a734f362143a0eae2cf
Saleck3 <aledagonale@gmail.com>
2024-05-30 21:30:00 -0300

    Working form for refactor and back with 400

---
+++ src/components/ui/checkbox.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { Slot } from '@radix-ui/react-slot';
+import { cva, type VariantProps } from 'class-variance-authority';
+import { cn } from '@/lib/utils';
+import { boolean } from 'zod';
+
+const checkboxVariants = cva(
+	'w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600',
+	{
+		variants: {
+			variant: {
+				default: 'w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600',		
+			},
+		},
+		defaultVariants: {
+			variant: 'default',
+		},
+	}
+);
+
+export interface CheckboxProps
+ extends React.InputHTMLAttributes<HTMLInputElement>, VariantProps<typeof checkboxVariants>{
+	asChild?: boolean;
+}
+
+const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
+	({ className, variant, asChild = false, ...props }, ref) => {
+		const Comp = asChild ? Slot : 'input';
+		return (
+			<Comp
+				className={cn(checkboxVariants({ variant, className }))}
+				ref={ref}
+				type= "checkbox"
+				
+				{...props}
+			/>
+		);
+	}
+);
+Checkbox.displayName = 'Checkbox';
+export { Checkbox, checkboxVariants };
