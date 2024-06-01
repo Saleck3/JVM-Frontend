40d02ed6c5f305724569391fd80ef1d91642b1fb
Laura <Laura@mail.com>
2024-06-01 02:01:42 -0300

    feat: se refactoriza register

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,15 +1,8 @@
-import type { Metadata } from 'next';
+import Form from './form'
 import Image from 'next/image';
 import { Card } from '@/components/ui/card';
-import { RegisterForm } from './form';
-
-export const metadata: Metadata = {
-    title: 'Register',
-    description: 'Create an account to start using our app',
-};
 
 export default function Register() {
-
   return (
     <main className="flex items-center justify-center min-h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
     <div className="flex flex-col md:flex-row gap-8 max-w-5xl w-full p-4 justify-center items-center">
@@ -26,13 +19,13 @@ export default function Register() {
       </div>
       <div className="flex justify-center items-center md:w-1/2">
         <div className="max-w-lg">
-          <Card className="p-5">
-            <RegisterForm />
+        <Card className="p-5">
+          <Form/>
           </Card>
         </div>
       </div>
     </div>
   </main>
+  )
+}
 
-  );
-};
