14d61f562f85a67c9a9e2f005abf837bd7e95b6d
Saleck3 <aledagonale@gmail.com>
2024-06-03 18:06:19 -0300

    Feature funcionando con locales

--- src/app/shared/components/TextToSpeech.tsx
+++ src/app/shared/components/TextToSpeech.tsx
@@ -1,53 +1,30 @@
 'use client';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
-
 
 type Props = {
     texto: string;
 };
 
 export default function TextToSpeech({ texto }: Props) {
-
-    const [synth, setSynth] = useState(window.speechSynthesis);
-
-    //Esto previene que use el idioma por default cuando ingresa
-    useEffect(() => {
-        return () => {
-            synth.cancel();
-        };
-    });
-
     const handlePlay = () => {
+        const synth = window.speechSynthesis;
         const utterance = new SpeechSynthesisUtterance(texto);
-        var selectedVoice;
 
-        //Edge
-        if (!selectedVoice) {
-            selectedVoice = synth.getVoices().find((v) => v.name === "Microsoft Valentina Online (Natural) - Spanish (Uruguay)");
-            utterance.rate = 0.9;
-        }
-        if (!selectedVoice) {
-            selectedVoice = synth.getVoices().find((v) => v.name === "Microsoft Tomas Online (Natural) - Spanish (Argentina)");
-            utterance.rate = 0.9;
-        }
+        //Filtro las voces en español
+        var langRegex = /^es(-[a-z]{2})?$/i;
+        var voices = synth.getVoices().filter((voice) => langRegex.test(voice.lang));
+        console.log(voices);
 
-        //Chrome
+        //Se podria reemplazar con una variable que dependa del player
+        var selectedVoice = voices.find((v) => v.name === "Google español");
         if (!selectedVoice) {
-            selectedVoice = synth.getVoices().find((v) => v.name === "Google español");
-            utterance.rate = 0.8;
-        }
-
-        if (!selectedVoice) {
-            //Si no encuentro ninguna elegida por nosotros, seteo la primera que sea de español
-            var langRegex = /^es(-[a-z]{2})?$/i;
-            var voices = synth.getVoices().filter((voice) => langRegex.test(voice.lang));
+            //Si no encuentro la que busque, seteo la que tenga
             selectedVoice = voices[0];
-            utterance.rate = 0.9;
         }
 
         utterance.voice = selectedVoice;
         utterance.pitch = 1.2; //Tono, lo hace mas o menos aflautado
+        utterance.rate = 0.9; //Velocidad de la voz
         utterance.volume = 1;
         synth.speak(utterance);
     };
