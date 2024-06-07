a070a47a1bcd4a0389c2f149ab0e8f718a8ef42a
Laura <Laura@mail.com>
2024-06-07 00:38:54 -0300

    se corrige manejo de errores

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -7,6 +7,7 @@ export const FormDataSchema = z.object({
     .string()
     .email("Formato de email incorrecto")
     .min(4, "No olvides tu email"),
+  player_name: z.string().min(2, 'Debes elegir un nombre de jugador'),
   password: z.string().min(8, "La clave debe contener al menos 8 caracteres"),
   repeatPassword: z.string().min(8, 'Las claves deben coincidir'),
   terms: z.preprocess((x) => x === "on", z.boolean()).refine(val => val === true, {
@@ -16,3 +17,18 @@ export const FormDataSchema = z.object({
   path: ['repeatPassword'],
   message: 'Las contraseñas no coinciden',
 });
+
+export const FormAddPlayerSchema = z.object({
+ 
+  player_name: z.string().min(2, 'Debes elegir un nombre de jugador'),
+  birth_date: z.string().refine((dateString) => {
+    const currentDate = new Date();
+    const minDate = new Date(currentDate.getFullYear() - 120, currentDate.getMonth(), currentDate.getDate());
+    const maxDate = new Date(currentDate.getFullYear() - 5, currentDate.getMonth(), currentDate.getDate());
+    const dateOfBirth = new Date(dateString);
+    return dateOfBirth >= minDate && dateOfBirth <= maxDate;
+  }, {
+    message: 'La fecha de nacimiento debe estar entre 1900 y 5 años antes de la fecha actual.'
+  }),
+});
+
