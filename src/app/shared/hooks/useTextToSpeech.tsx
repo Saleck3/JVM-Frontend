013d1026219aa1f6680bd805c558dfe56cf32937
LJVazquez <ljvazquez00@gmail.com>
2024-06-07 20:36:44 -0300

    refact: useTTS minor refact

--- src/app/shared/hooks/useTextToSpeech.tsx
+++ src/app/shared/hooks/useTextToSpeech.tsx
@@ -2,46 +2,58 @@
 
 import { useEffect, useState } from 'react';
 
-const VOICES_LIST = [
-    "Microsoft Valentina Online (Natural) - Spanish (Uruguay)",
-    "Microsoft Tomas Online (Natural) - Spanish (Argentina)",
-    "Google español"
-]
-export default function useTextToSpeech(texto: string) {
+const PREFERRED_VOICES = [
+	'Microsoft Valentina Online (Natural) - Spanish (Uruguay)',
+	'Microsoft Tomas Online (Natural) - Spanish (Argentina)',
+	'Google español',
+];
 
-    const [synth, setSynth] = useState<SpeechSynthesis | null>(null);
-    const [foundVoice, setVoice] = useState<SpeechSynthesisVoice | null>(null);
+export default function useTextToSpeech(text: string) {
+	const [synth, setSynth] = useState<SpeechSynthesis | null>(null);
+	const [foundVoice, setVoice] = useState<SpeechSynthesisVoice | null>(null);
 
-    //Esto previene que use el idioma por default cuando ingresa
-    useEffect(() => {
-        setSynth(window.speechSynthesis);
+	useEffect(() => {
+		const _synth = window.speechSynthesis;
+		setSynth(_synth);
 
-        const prefferedVoice = synth?.getVoices().find(voice => voice.name === "Microsoft Valentina Online (Natural) - Spanish (Uruguay)") || null;
+		const prefferedVoice =
+			_synth
+				?.getVoices()
+				.find(
+					(voice) =>
+						voice.name ===
+						'Microsoft Valentina Online (Natural) - Spanish (Uruguay)'
+				) || null;
 
-        const backupVoice = synth?.getVoices().find(voice => VOICES_LIST.includes(voice.name)) || null;
-        //Si no encuentro ninguna elegida por nosotros, seteo la primera que sea de español
-        const langRegex = /^es(-[a-z]{2})?$/i;
-        const defaultVoice = synth?.getVoices().find((voice) => langRegex.test(voice.lang)) || null;
+		const backupVoice =
+			_synth
+				?.getVoices()
+				.find((voice) => PREFERRED_VOICES.includes(voice.name)) || null;
 
-        setVoice(prefferedVoice || backupVoice || defaultVoice);
+		const langRegex = /^es(-[a-z]{2})?$/i;
+		const defaultVoice =
+			_synth?.getVoices().find((voice) => langRegex.test(voice.lang)) || null;
 
-        return () => {
-            synth?.cancel();
-        };
-    });
+		setVoice(prefferedVoice || backupVoice || defaultVoice);
 
-    const handlePlay = () => {
-        if (synth?.speaking) {
-            synth?.cancel();
-            return;
-        }
-        const utterance = new SpeechSynthesisUtterance(texto);
-        utterance.voice = foundVoice!;
-        utterance.pitch = 1.2; //Tono, lo hace mas o menos aflautado
-        utterance.volume = 1;
+		return () => {
+			_synth.cancel();
+			synth?.cancel();
+		};
+	}, [synth]);
 
-        synth?.speak(utterance);
-    };
+	const handlePlay = () => {
+		if (synth?.speaking) {
+			synth?.cancel();
+			return;
+		}
+		const utterance = new SpeechSynthesisUtterance(text);
+		utterance.voice = foundVoice!;
+		utterance.pitch = 1.2;
+		utterance.volume = 1;
 
-    return [handlePlay];
+		synth?.speak(utterance);
+	};
+
+	return [handlePlay];
 }
