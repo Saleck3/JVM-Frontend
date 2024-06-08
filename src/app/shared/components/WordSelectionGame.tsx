a0c53e62fa989cc777fdcf5777ab27bb5be9e3aa
Saleck3 <aledagonale@gmail.com>
2024-06-08 20:07:45 -0300

    Agregado boton de tts a titulo de juegos NO_IA

--- src/app/shared/components/WordSelectionGame.tsx
+++ src/app/shared/components/WordSelectionGame.tsx
@@ -5,6 +5,7 @@ import { ImageSelection } from '@/app/shared/types/games.type';
 import { useState } from 'react';
 import Image from 'next/image';
 import GameLayout from './GameLayout';
+import useTextToSpeech from '../hooks/useTextToSpeech';
 
 interface Props extends ImageSelection {
 	onWrongAnswer: () => void;
@@ -47,12 +48,14 @@ const WordSelectionGame = (props: Props): JSX.Element => {
 			? 'gray'
 			: 'secondary';
 	};
+	const [playInstructions] = useTextToSpeech("Para completar el juego, seleccioná con el mouse lo que falte en la palabra.");
 
 	return (
 		<GameLayout
 			gameFinished={gameFinished}
 			handleNextButton={handleNextButton}
 			title="Selecciona la palabra"
+			instructionsTTS={playInstructions}
 		>
 			<div className="mb-6 text-center flex-1 flex flex-col gap-8">
 				<div className="flex-1 relative">
