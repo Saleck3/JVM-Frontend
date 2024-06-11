6503943f5c6125769fc12364c38396304faf4c40
Saleck3 <aledagonale@gmail.com>
2024-06-10 22:22:29 -0300

    Movido server a services

--- src/app/shared/services/addPlayer.service.ts
+++ src/app/shared/services/addPlayer.service.ts
@@ -1,6 +1,9 @@
-import { FormAddPlayerSchema } from "../shared/schemas/schema";
+'use server;'
 
-export async function addEntry(state: any, data: FormData) {
+import { FormAddPlayerSchema } from "../schemas/schema";
+import { getSsrUtils } from '@/lib/utils';
+
+export async function addPlayer(state: any, data: FormData) {
   const result = FormAddPlayerSchema.safeParse({
     player_name: data.get("player"),
     birth_date: data.get("birthdate"),
@@ -11,6 +14,9 @@ export async function addEntry(state: any, data: FormData) {
   }
 
   if (result.success) {
+    const ssrUtils = await getSsrUtils();
+    const token = ssrUtils.getAccessTokenSsr();
+
     try {
       const response = await fetch(
         `${process.env.NEXT_PUBLIC_FRONTEND_URL}/api/addPlayer`,
@@ -32,22 +38,26 @@ export async function addEntry(state: any, data: FormData) {
         window.location.href = `${process.env.NEXT_PUBLIC_FRONTEND_URL}/players`;
         return;
       }
-      
-      if (response.status===400) {
+
+      if (response.status === 400) {
         console.log("soy un 400 del ts")
-         return {
-            error: {
-              email: { _errors: [""] },
-            },
-          };
-        } else {
-          return {
-            error: {
-              general: { _errors: ["Error en la solicitud"] },
-            },
-          };
-        }
+        return {
+          error: {
+            player_name: { _errors: [] },
+            birth_date: { _errors: [] },
+            general: { _errors: [""] },
+          },
+        };
+      } else {
+        return {
+          error: {
+            player_name: { _errors: [] },
+            birth_date: { _errors: [] },
+            general: { _errors: ["Error en la solicitud"] },
+          },
+        };
       }
+    }
     catch (errorBack: any) {
       console.error("Detailed error:", errorBack);
       return { errorBack: errorBack.message };
