3a4eb636f814ebe19dbd2c6ebd52109f6f6353fd
Saleck3 <aledagonale@gmail.com>
2024-06-10 21:50:42 -0300

    Movido service register a carpeta services

--- src/app/auth/register/form.tsx
+++ src/app/auth/register/form.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useFormState, useFormStatus } from "react-dom";
-import { addEntry } from "../../shared/services/register.service";
+import { registerUser } from "@/app/shared/services/register.service";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
