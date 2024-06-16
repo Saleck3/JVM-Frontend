3ed5b65ee38e464c3c02910a168d9c827d802416
Saleck3 <aledagonale@gmail.com>
2024-06-15 23:56:09 -0300

    Fix tts con distinta voz la primera vez

--- src/app/shared/hooks/useTextToSpeech.tsx
+++ src/app/shared/hooks/useTextToSpeech.tsx
@@ -10,31 +10,10 @@ const PREFERRED_VOICES = [
 
 export default function useTextToSpeech(text: string) {
 	const [synth, setSynth] = useState<SpeechSynthesis | null>(null);
-	const [foundVoice, setVoice] = useState<SpeechSynthesisVoice | null>(null);
 
 	useEffect(() => {
 		const _synth = window.speechSynthesis;
 		setSynth(_synth);
-
-		const prefferedVoice =
-			_synth
-				?.getVoices()
-				.find(
-					(voice) =>
-						voice.name ===
-						'Microsoft Valentina Online (Natural) - Spanish (Uruguay)'
-				) || null;
-
-		const backupVoice =
-			_synth
-				?.getVoices()
-				.find((voice) => PREFERRED_VOICES.includes(voice.name)) || null;
-
-		const langRegex = /^es(-[a-z]{2})?$/i;
-		const defaultVoice =
-			_synth?.getVoices().find((voice) => langRegex.test(voice.lang)) || null;
-
-		setVoice(prefferedVoice || backupVoice || defaultVoice);
 
 		return () => {
 			_synth.cancel();
@@ -48,13 +27,26 @@ export default function useTextToSpeech(text: string) {
 			return;
 		}
 		const utterance = new SpeechSynthesisUtterance(text);
-		utterance.voice = foundVoice!;
+		utterance.voice = findVoice();
 		utterance.rate = 0.9;
 		utterance.pitch = 1.2;
 		utterance.volume = 1;
 
 		synth?.speak(utterance);
 	};
+
+	const findVoice = () => {
+		const _synth = window.speechSynthesis;
+		const prefferedVoice = _synth?.getVoices().find((voice) =>
+			voice.name === 'Microsoft Valentina Online (Natural) - Spanish (Uruguay)') || null;
+
+		const backupVoice = _synth?.getVoices().find((voice) => PREFERRED_VOICES.includes(voice.name)) || null;
+
+		const langRegex = /^es(-[a-z]{2})?$/i;
+		const defaultVoice =
+			_synth?.getVoices().find((voice) => langRegex.test(voice.lang)) || null;
+		return prefferedVoice || backupVoice || defaultVoice;
+	}
 
 	return [handlePlay];
 }
